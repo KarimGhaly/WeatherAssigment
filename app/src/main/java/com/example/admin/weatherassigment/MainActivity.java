8167d0502d3b249f1df468816400c4d65ceb974c
Karim Ghaly <ghaly.karim@outlook.com>
2017-09-19 09:34:56 -0400

    Add EditText to enter the ZipCode

--- app/src/main/java/com/example/admin/weatherassigment/MainActivity.java
+++ app/src/main/java/com/example/admin/weatherassigment/MainActivity.java
@@ -4,7 +4,10 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.admin.weatherassigment.model.WeatherClass;
 
@@ -18,40 +21,62 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "WeatherTAG";
+    private EditText etZipCode;
+    private TextView txtCountry;
+    private TextView txtCity;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView txtCity = (TextView) findViewById(R.id.txtCity);
-        final TextView txtCountry = (TextView) findViewById(R.id.txtCountry);
+        txtCity = (TextView) findViewById(R.id.txtCity);
+        txtCountry = (TextView) findViewById(R.id.txtCountry);
+        etZipCode = (EditText) findViewById(R.id.txtZIPCode);
 
 
-        Call<WeatherClass> getWeather = RetrofitHelper.getWeatherDetails("30067","bc82aea40a3bad00df74f74a9bd8aa23","imperial");
 
 
-        getWeather.enqueue(new Callback<WeatherClass>() {
-            @Override
-            public void onResponse(Call<WeatherClass> call, final Response<WeatherClass> response) {
-                final WeatherClass weatherClass = response.body();
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        txtCity.setText(weatherClass.getCity().getName());
-                        txtCountry.setText(weatherClass.getCity().getCountry());
-                        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager1);
-                        MyAdapter myAdapter = new MyAdapter(MainActivity.this,weatherClass);
-                        viewPager.setAdapter(myAdapter);
+    }
+
+    public void SearchWeather(View view) {
+        String ZipCode = etZipCode.getText().toString();
+        if(!ZipCode.isEmpty())
+        {
+            Call<WeatherClass> getWeather = RetrofitHelper.getWeatherDetails(ZipCode,"bc82aea40a3bad00df74f74a9bd8aa23","imperial");
+            getWeather.enqueue(new Callback<WeatherClass>() {
+                @Override
+                public void onResponse(Call<WeatherClass> call, final Response<WeatherClass> response) {
+                    if(response.isSuccessful()) {
+                        final WeatherClass weatherClass = response.body();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                txtCity.setText(weatherClass.getCity().getName());
+                                txtCountry.setText(weatherClass.getCity().getCountry());
+                                ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager1);
+                                MyAdapter myAdapter = new MyAdapter(MainActivity.this, weatherClass);
+                                viewPager.setAdapter(myAdapter);
+                            }
+                        });
                     }
-                });
-           }
+                    else
+                    {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(MainActivity.this, "Wrong Zip Code", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                }
 
-            @Override
-            public void onFailure(Call<WeatherClass> call, Throwable t) {
+                @Override
+                public void onFailure(Call<WeatherClass> call, Throwable t) {
+                    Log.d(TAG, "onFailure: "+t.toString());
+                }
+            });
+        }
 
-            }
-        });
-//
     }
 }
